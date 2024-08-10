31865b16939b53f0c20b1d3f94b642b8f85df01c
MJohnson <48001236+screencancer@users.noreply.github.com>
2024-08-10 15:07:42 -0400

    test for unknown char

--- app/main.py
+++ app/main.py
@@ -51,9 +51,7 @@ def main():
             print("SEMICOLON ; null")
         elif c == "/":
             print("SLASH / null")
-        else:
-            if not c:
-                break
+        elif c != "" or c != "":
             error_message = f"[line {findlinenum(file_contents, c)}] " + f"Error: Unexpected character: {c}"
             sys.stderr.write(error_message)
             sys.exit(65)
