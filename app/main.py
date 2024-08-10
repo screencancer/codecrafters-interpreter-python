0b10ff1f24c0017f70a6b9fc482bffe2af427d37
MJohnson <48001236+screencancer@users.noreply.github.com>
2024-08-10 15:28:26 -0400

    test for unknown char

--- app/main.py
+++ app/main.py
@@ -52,8 +52,7 @@ def main():
         elif c == "/":
             print("SLASH / null")
         else:
-            print(findlinenum(file_contents, c))
-            if findlinenum(file_contents, c) > 0:
+            if not c == '\0':
                 error_message = f"[line {findlinenum(file_contents, c)}] " + f"Error: Unexpected character: {c}"
                 sys.stderr.write(error_message)
                 sys.exit(65)
