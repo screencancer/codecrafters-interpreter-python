5a483899a85298a040e5762b3ddb8766dee74ce3
MJohnson <48001236+screencancer@users.noreply.github.com>
2024-08-10 14:37:06 -0400

    braces scanning

--- app/main.py
+++ app/main.py
@@ -29,6 +29,15 @@ def main():
             print("LEFT_BRACE { null")
         elif c == "}":
             print("RIGHT_BRACE } null")
+        elif c == "*":
+            print("STAR * null")
+        elif c == ".":
+            print("DOT . null")
+        elif c == ",":
+            print("COMMA , null")
+        elif c == "+":
+            print("PLUS + null")
+
 
     print("EOF  null")
 
