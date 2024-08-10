4e59176c1a12c25f5a8eca1a68d151433f86e9b9
MJohnson <48001236+screencancer@users.noreply.github.com>
2024-08-10 16:38:08 -0400

    detecting equals

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ def main():
     pointer = 0
     while pointer < len(file_contents):
         c = file_contents[pointer]
-        print(c)
+        print(pointer)
         pointer += 1
         if c == "(":
             print("LEFT_PAREN ( null")
